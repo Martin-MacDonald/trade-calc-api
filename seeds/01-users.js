a8c5612217609fcab66a04d394f542fa9980756d
Martin-MacDonald <mmacdo54@caledonian.ac.uk>
2019-03-03 17:48:36 +0000

    Fixed seeds error

--- seeds/01-users.js
+++ seeds/01-users.js
@@ -4,19 +4,16 @@ exports.seed = knex => knex('userVerifications')
 .then(() => knex('users')
   .insert([
     {
-      id: 1,
       username: 'mmacdo54',
       password: '$2y$12$uzOuBMbZZvKXBT4UXCplj.7x5vjTZm98FE9dsIg20h05ZZC4wB7ly',
       email: 'test@gmail.com',
     },
     {
-      id: 2,
       username: 'smacdo54',
       password: '$2y$12$uzOuBMbZZvKXBT4UXCplj.7x5vjTZm98FE9dsIg20h05ZZC4wB7ly',
       email: 'test2@gmail.com',
     },
     {
-      id: 3,
       username: 'bmacdo54',
       password: '$2y$12$uzOuBMbZZvKXBT4UXCplj.7x5vjTZm98FE9dsIg20h05ZZC4wB7ly',
       email: 'test3@gmail.com',
--- seeds/02-userVerifications.js
+++ seeds/02-userVerifications.js
@@ -1,17 +1,14 @@
 exports.seed = knex => knex('userVerifications')
   .insert([
     {
-      id: 1,
       userId: 1,
       verificationKey: '4132928f44b36e852f4aed3dd269c59834eb0abf241342bce97cecba43fd0ef082439c209fa9f9c177b35f057b99dd24',
     },
     {
-      id: 2,
       userId: 2,
       verificationKey: '5132928f44b36e852f4aed3dd269c59834eb0abf241342bce97cecba43fd0ef082439c209fa9f9c177b35f057b99dd24',
     },
     {
-      id: 3,
       userId: 3,
       verificationKey: '6132928f44b36e852f4aed3dd269c59834eb0abf241342bce97cecba43fd0ef082439c209fa9f9c177b35f057b99dd24',
     },
